f1e3964865318ac463bb8e113c4eaf9264869183
Nicholas Boyko <uthorrr@gmail.com>
2016-03-26 15:04:02 -0400

    Added files via upload
    Uploaded version without keys included

---
+++ main_scrubbed.py
@@ -0,0 +1,45 @@
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import datetime
+
+auth = tweepy.OAuthHandler(consumer, consumersecret)
+auth.set_access_token(access, accesssecret)
+
+api = tweepy.API(auth)
+
+currentfile = str(datetime.date.today().day) + str(datetime.date.today().month) + "tweets.txt"
+
+# currentfile = "tweets.txt"
+
+try:
+    with(open(currentfile, "r")) as f:
+        print("TESTED FILE " + currentfile + " SUCCESSFULLY")
+except:
+    try:
+        with(open(currentfile, "w")) as f:
+            f.write(currentfile + "TWEETS FOLLOW \n")
+            print("CREATED FILE " + currentfile)
+    except:
+        print("FILE ERROR ON " + currentfile)
+
+class DCIWListener(StreamListener):
+    def on_status(self, status):
+        if True:
+            print(status.text)
+            with(open(currentfile, "a")) as f:
+                f.write(status.text)
+                f.write("|")
+                f.write(str(status.user.name))
+                f.write('\n')
+    def on_error(self, status_code):
+        print(str(sys.stderr), 'Encountered error with status code:',str(status_code))
+        return True # Don't kill the stream
+    def on_timeout(self):
+        print(str(sys.stderr), 'Timeout...')
+        return True # Don't kill the stream
+
+keywords = ['Academy', 'The Academy', 'Blue Devils', '#dci', '@dci', '@DCI', 'Blue Knights', 'Knights', 'Blue Stars', 'Stars', 'Bluecoats', 'Bloo', 'Boston Crusaders', 'Boston', 'The Cadets', 'Cadets', 'cadets', 'the cadets', 'Carolina Crown', 'Carolina', 'Crown', 'Cascades', 'The Cavaliers', 'Cavaliers', 'Colts', 'Crossmen', 'Jersey Surf', 'Surf', 'Madison Scouts', 'The Scouts', 'Mandarins', 'Oregon Crusaders', 'Pacific Crest', 'Crest', 'Phantom Regiment', 'Phantom', 'Pioneer', 'Santa Clara Vanguard', 'SCV', 'Spirit of Atlanta', 'Troopers']
+
+twitter_stream = Stream(auth, DCIWListener())
+twitter_stream.filter(track=["dci2016"])
